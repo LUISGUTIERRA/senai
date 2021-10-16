692f00e06102b0332a8bdfac010d3d481f99f816
LUISGUTIERRA <luisgutierrajr@gmail.com>
2021-10-15 21:40:22 -0300

    visualcode

---
+++ aula2/01.js
@@ -0,0 +1,9 @@
+//primeiro programa
+//console.log('olá mundo');
+var nome = 'Luis';
+var sobrenome = 'Gutierra'; 
+var nomecompleto;
+/*nomecompleto = nome + sobrenome;
+console.log(nome + ' ' + sobrenome);*/ 
+var idade = 30;
+console.log ('Meu nome é '+ ' '+ nome + ' ' + 'tenho:'  + idade);
---
+++ aula2/03.js
@@ -0,0 +1,18 @@
+/*var valorum = 5;
+var valordois = 4;
+var valortres = 9;
+var total;
+total = (valorum + valordois) * valortres;
+console.log(total);*/
+
+/*var  a = 5;
+var  b = 2; 
+var c = 10;
+total = ( b * a - a) / c;
+console.log(total);*/ 
+
+var a = 20
+var b = 10
+var c = 5 
+total = ( a * a / b ) / c;
+console.log(total);
